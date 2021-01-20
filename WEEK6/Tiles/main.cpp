95adbfdb74199f181b3136f267f3743094539923
Dragoljub Djuric <djuric2103@gmail.com>
2021-01-20 18:16:07 +0100

    Add files via upload

---
+++ WEEK6/Tiles/main.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, long,
+        boost::property<boost::edge_residual_capacity_t, long,
+            boost::property<boost::edge_reverse_t, traits::edge_descriptor>>>> graph;
+typedef traits::vertex_descriptor vertex_desc;
+typedef traits::edge_descriptor edge_desc;
+class edge_adder {
+  graph &G;
+ public:
+  explicit edge_adder(graph &G) : G(G) {}
+  void add_edge(int from, int to, long capacity) {
+    auto c_map = boost::get(boost::edge_capacity, G);
+    auto r_map = boost::get(boost::edge_reverse, G);
+    const auto e = boost::add_edge(from, to, G).first;
+    const auto rev_e = boost::add_edge(to, from, G).first;
+    c_map[e] = capacity;
+    c_map[rev_e] = 0; // reverse edge has no capacity!
+    r_map[e] = rev_e;
+    r_map[rev_e] = e;
+  }
+};
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    long w, h; cin >> n;
+    while(n--){
+        cin >> w >> h;
+        graph G(2*w*h);
+        edge_adder adder(G);
+        const vertex_desc v_source = boost::add_vertex(G);
+        const vertex_desc v_sink = boost::add_vertex(G);
+        vector<vector<int>> mat(h, vector<int>(w, 0));
+        char c;
+        long k = 0, fields = 0;
+        for(int i = 0; i < h; ++i){
+            for(int j = 0; j < w; ++j){
+                cin >> c;
+                if(c == '.'){
+                    mat[i][j] = 1;
+                    adder.add_edge(2*(w*i + j), 2*(w*i + j) + 1, 1);
+                    ++fields;
+                    if(i % 2 == j % 2) adder.add_edge(v_source, 2*(w*i + j), 1);
+                    else adder.add_edge(2*(w*i + j) + 1, v_sink, 1);
+                    if(i != 0 && mat[i-1][j] == 1){
+                        adder.add_edge(2*(w*i + j) + 1, 2*(w*(i - 1) + j), 1);
+                        adder.add_edge(2*(w*(i - 1) + j) + 1, 2*(w*i + j), 1);
+                    }
+                    if(j != 0 && mat[i][j - 1] == 1){
+                        adder.add_edge(2*(w*i + j) + 1, 2*(w*i + j - 1), 1);
+                        adder.add_edge(2*(w*i + j - 1) + 1, 2*(w*i + j), 1);
+                    }
+                    if(i != h-1 && mat[i+1][j] == 1){
+                        adder.add_edge(2*(w*i + j) + 1, 2*(w*(i + 1) + j), 1);
+                        adder.add_edge(2*(w*(i + 1) + j) + 1, 2*(w*i + j), 1);
+                    }
+                    if(j != w-1 && mat[i][j+1] == 1){
+                        adder.add_edge(2*(w*i + j) + 1, 2*(w*i + j + 1), 1);
+                        adder.add_edge(2*(w*i + j + 1) + 1, 2*(w*i + j), 1);
+                    }
+                }
+            }
+        }
+        long flow = boost::push_relabel_max_flow(G, v_source, v_sink);
+        cout << (2*flow == fields ? "yes\n" : "no\n");
+    }
+
+    return 0;
+}
